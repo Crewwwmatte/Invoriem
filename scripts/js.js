2ae39138a44098a5419dfbdd773dbbe04f8560a1
DaggerHashimmoto <DaggerHashimmoto@gmail.com>
2022-08-26 23:31:04 +0500

    //video

--- scripts/js.js
+++ scripts/js.js
@@ -4,3 +4,27 @@ $(document).ready(function() {
         margin: 48
     });
 });
+
+const videoBtn = document.querySelector('#video-btn')
+const vidPic = document.querySelector('.video-pic')
+const videoWrapper = document.querySelector('.video-video')
+const video = document.querySelector('#video-file')
+
+videoBtn.addEventListener('click', function () {
+    vidPic.classList.add('none')
+    videoWrapper.classList.remove('video-overlay')
+
+    if (video.paused) {
+        function ply() {
+            videoBtn.classList.add('none')
+            video.play()
+        }
+        ply()
+    }
+})
+
+video.addEventListener('click', function () {
+    videoBtn.classList.remove('none')
+    videoWrapper.classList.add('video-overlay')
+    video.pause()
+})
